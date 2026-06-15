6c29cbb0f606681673d7dfaa98963a845e1ce4ea
najak <>
2026-06-15 21:34:48 +0900

    커밋 next와 Prev 되도록 수정

--- src/services/lhApi.js
+++ src/services/lhApi.js
@@ -4,21 +4,21 @@ import { getLocationCode } from "../utils/locationUtils";
 const SERVICE_KEY = 'f471c3a21df119bf22449a724bf67affb5f3387c2f29c661cda32b4b5031169a';
 
 export async function fetchLeaseNotices(params = {}) {
-    const PAGE = 1;
-    const PG_SZ = 30;
-    const PAN_ST_DT = params.PAN_ST_DT ? params.PAN_ST_DT.replace(/-\./g, "") : getMonthsAgoString(2);
-    const PAN_ED_DT = params.PAN_ED_DT ? params.PAN_ED_DT.replace(/-\./g, "") : getTodayString();
-    const CNP_CD = getLocationCode(params.locationName || "서울");
+    const PAGE = params.PAGE || 1;
+    const PG_SZ = params.PG_SZ || 30;
+    const PAN_ST_DT = params.PAN_ST_DT ? params.PAN_ST_DT.replace(/[-.]/g, "") : getMonthsAgoString(2);
+    const PAN_ED_DT = params.PAN_ED_DT ? params.PAN_ED_DT.replace(/[-.]/g, "") : getTodayString();
+    const CNP_CD = params.locationName ? getLocationCode(params.locationName) : "";
     const PAN_SS = params.PAN_SS || "";
 
     const url = `https://us-central1-lhhousenoti.cloudfunctions.net/lhLeaseNotice?serviceKey=${SERVICE_KEY}&PAGE=${PAGE}&PG_SZ=${PG_SZ}&PAN_ST_DT=${PAN_ST_DT}&PAN_ED_DT=${PAN_ED_DT}&CNP_CD=${CNP_CD}&PAN_SS=${PAN_SS}`;
 
     const response = await fetch(url);
 
-    if(!response.ok) {
-        throw new Error('API 호출 실패: ${response.status}');
+    if (!response.ok) {
+        throw new Error(`API 호출 실패: ${response.status}`);
     }
-    const json = await response.json()
+    const json = await response.json();
 
     const resultBlock = json.find((block) => block.dsList);
     const header = json.find((block) => block.resHeader)?.resHeader?.[0];
